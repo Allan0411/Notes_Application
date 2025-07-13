c11618664c23aa69602b98a36230f4bc6223f359
Allan0411 <85943831+Allan0411@users.noreply.github.com>
2025-07-13 09:44:49 +0530

    Delete src directory

--- src/Accounts.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Accounts() {
-    return (
-        <div>
-            <h1>Hi</h1>
-        </div>
-    );
-}
--- src/Homepage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Homepage = () => {
-    return <h2>Welcome to BudgetWise Home!</h2>;
-};
-
-export default Homepage;
