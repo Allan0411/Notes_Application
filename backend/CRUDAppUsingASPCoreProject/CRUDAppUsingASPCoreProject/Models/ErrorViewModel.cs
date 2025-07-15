391929b87785e36395fc45362fb2fb17002c7b76
Allan0411 <85943831+Allan0411@users.noreply.github.com>
2025-07-15 23:20:54 +0530

    Delete backend/CRUDAppUsingASPCoreProject directory

--- backend/CRUDAppUsingASPCoreProject/CRUDAppUsingASPCoreProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CRUDAppUsingASPCoreProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
